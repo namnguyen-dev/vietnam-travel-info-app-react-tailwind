b72fab42f99edaf5a587b7f7b50ca7e51512d559
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2022-04-25 13:17:55 +0700

    add Hero section to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Hero } from '../components';
 
 const HomePage = () => {
-  return <div>HomePage </div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default HomePage;
